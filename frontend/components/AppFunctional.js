a37961ae71584bf5a259602b9505a910b74f18ea
Victor Brambila <victorbrambila22@gmail.com>
2022-03-10 21:42:41 -0800

    func buttons coordinates

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,26 +1,51 @@
 import React, { useState } from 'react'
 
-const initialState = []
+const initialState = {
+  x: 2,
+  y: 2
+}
 
 const initialCount = 0;
 
 export default function AppFunctional(props) {
   const [grid, setGrid] = useState(initialState)
   const [count, setCount] = useState(initialCount);
+  console.log(grid, "grid");
 
-  const increaseCount = () => {
+  const leftBtn = () => {
     const increase = count + 1;
     setCount(increase);
+    const move = grid.x - 1;
+    setGrid({ x: move, y: grid.y });
+  }
+  const rightBtn = () => {
+    const increase = count + 1;
+    setCount(increase);
+    const move = grid.x + 1;
+    setGrid({ x: move, y: grid.y });
+  }
+  const upBtn = () => {
+    const increase = count + 1;
+    setCount(increase);
+    const move = grid.y - 1;
+    setGrid({ x: grid.x, y: move });
+  }
+  const downBtn = () => {
+    const increase = count + 1;
+    setCount(increase);
+    const move = grid.y + 1;
+    setGrid({ x: grid.x, y: move });
   }
 
   const resetCount = () => {
-    setCount(initialCount)
+    setCount(initialCount);
+    setGrid(initialState);
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
+        <h3 id="coordinates">Coordinates ({ grid.x }, { grid.y })</h3>
         <h3 id="steps">You moved { count } times</h3>
       </div>
       <div id="grid">
@@ -38,10 +63,10 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button onClick={increaseCount} id="left">LEFT</button>
-        <button onClick={increaseCount} id="up">UP</button>
-        <button onClick={increaseCount} id="right">RIGHT</button>
-        <button onClick={increaseCount} id="down">DOWN</button>
+        <button onClick={leftBtn} id="left">LEFT</button>
+        <button onClick={upBtn} id="up">UP</button>
+        <button onClick={rightBtn} id="right">RIGHT</button>
+        <button onClick={downBtn} id="down">DOWN</button>
         <button onClick={resetCount} id="reset">reset</button>
       </div>
       <form>
