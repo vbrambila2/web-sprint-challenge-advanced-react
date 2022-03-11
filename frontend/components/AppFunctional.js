935f461446532982531d4ec24b3050d2066738f3
Victor Brambila <victorbrambila22@gmail.com>
2022-03-10 21:22:43 -0800

    func count increase

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialState = []
+
+const initialCount = 0;
 
 export default function AppFunctional(props) {
+  const [grid, setGrid] = useState(initialState)
+  const [count, setCount] = useState(initialCount);
+
+  const increaseCount = () => {
+    const increase = count + 1;
+    setCount(increase);
+  }
+
+  const resetCount = () => {
+    setCount(initialCount)
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="steps">You moved { count } times</h3>
       </div>
       <div id="grid">
         <div className="square"></div>
@@ -22,11 +38,11 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={increaseCount} id="left">LEFT</button>
+        <button onClick={increaseCount} id="up">UP</button>
+        <button onClick={increaseCount} id="right">RIGHT</button>
+        <button onClick={increaseCount} id="down">DOWN</button>
+        <button onClick={resetCount} id="reset">reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
