8a2963e3e23c54127b546aa621ef9bb061d96182
Victor Brambila <victorbrambila22@gmail.com>
2022-03-10 23:44:45 -0800

    func letter moves with button click

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -10,7 +10,8 @@ const initialCount = 0;
 export default function AppFunctional(props) {
   const [grid, setGrid] = useState(initialState)
   const [count, setCount] = useState(initialCount);
-  const [letter, setLetter] = useState('B')
+  
+  const letter = 'B';
 
   const leftBtn = () => {
     const increase = count + 1;
@@ -31,21 +32,33 @@ export default function AppFunctional(props) {
     if (five.classList.contains("active")) {
       five.classList.remove("active");
       four.classList.add("active");
+      four.innerHTML = letter;
+      five.innerHTML = "";
     } else if (six.classList.contains("active")) {
       six.classList.remove("active");
       five.classList.add("active");
+      five.innerHTML = letter;
+      six.innerHTML = "";
     } else if (two.classList.contains("active")) {
       two.classList.remove("active");
       one.classList.add("active");
+      one.innerHTML = letter;
+      two.innerHTML = "";
     } else if (three.classList.contains("active")) {
       three.classList.remove("active");
       two.classList.add("active");
+      two.innerHTML = letter;
+      three.innerHTML = "";
     } else if (eight.classList.contains("active")) {
       eight.classList.remove("active");
       seven.classList.add("active");
+      seven.innerHTML = letter;
+      eight.innerHTML = "";
     } else if (nine.classList.contains("active")) {
       nine.classList.remove("active");
       eight.classList.add("active");
+      eight.innerHTML = letter;
+      nine.innerHTML = "";
     }
   }
 
@@ -68,21 +81,33 @@ export default function AppFunctional(props) {
     if (five.classList.contains("active")) {
       five.classList.remove("active");
       six.classList.add("active");
+      six.innerHTML = letter;
+      five.innerHTML = "";
     } else if (four.classList.contains("active")) {
       four.classList.remove("active");
       five.classList.add("active");
+      five.innerHTML = letter;
+      four.innerHTML = "";
     } else if (one.classList.contains("active")) {
       one.classList.remove("active");
       two.classList.add("active");
+      two.innerHTML = letter;
+      one.innerHTML = "";
     } else if (two.classList.contains("active")) {
       two.classList.remove("active");
       three.classList.add("active");
+      three.innerHTML = letter;
+      two.innerHTML = "";
     } else if (seven.classList.contains("active")) {
       seven.classList.remove("active");
       eight.classList.add("active");
+      eight.innerHTML = letter;
+      seven.innerHTML = "";
     } else if (eight.classList.contains("active")) {
       eight.classList.remove("active");
       nine.classList.add("active");
+      nine.innerHTML = letter;
+      eight.innerHTML = "";
     }
   }
 
@@ -105,21 +130,33 @@ export default function AppFunctional(props) {
     if (five.classList.contains("active")) {
       five.classList.remove("active");
       two.classList.add("active");
+      two.innerHTML = letter;
+      five.innerHTML = "";
     } else if (eight.classList.contains("active")) {
       eight.classList.remove("active");
       five.classList.add("active");
+      five.innerHTML = letter;
+      eight.innerHTML = "";
     } else if (seven.classList.contains("active")) {
       seven.classList.remove("active");
       four.classList.add("active");
+      four.innerHTML = letter;
+      seven.innerHTML = "";
     } else if (four.classList.contains("active")) {
       four.classList.remove("active");
       one.classList.add("active");
+      one.innerHTML = letter;
+      four.innerHTML = "";
     } else if (nine.classList.contains("active")) {
       nine.classList.remove("active");
       six.classList.add("active");
+      six.innerHTML = letter;
+      nine.innerHTML = "";
     } else if (six.classList.contains("active")) {
       six.classList.remove("active");
       three.classList.add("active");
+      three.innerHTML = letter;
+      six.innerHTML = "";
     }
   }
 
@@ -142,21 +179,33 @@ export default function AppFunctional(props) {
     if (five.classList.contains("active")) {
       five.classList.remove("active");
       eight.classList.add("active");
+      eight.innerHTML = letter;
+      five.innerHTML = "";
     } else if (two.classList.contains("active")) {
       two.classList.remove("active");
       five.classList.add("active");
+      five.innerHTML = letter;
+      two.innerHTML = "";
     } else if (one.classList.contains("active")) {
       one.classList.remove("active");
       four.classList.add("active");
+      four.innerHTML = letter;
+      one.innerHTML = "";
     } else if (four.classList.contains("active")) {
       four.classList.remove("active");
       seven.classList.add("active");
+      seven.innerHTML = letter;
+      four.innerHTML = "";
     } else if (three.classList.contains("active")) {
       three.classList.remove("active");
       six.classList.add("active");
+      six.innerHTML = letter;
+      three.innerHTML = "";
     } else if (six.classList.contains("active")) {
       six.classList.remove("active");
       nine.classList.add("active");
+      nine.innerHTML = letter;
+      six.innerHTML = "";
     }
   }
 
@@ -164,8 +213,12 @@ export default function AppFunctional(props) {
     setCount(initialCount);
     setGrid(initialState);
 
-    document.querySelector(".active").classList.remove("active");
-    document.querySelector(".five").classList.add("active");
+    const active = document.querySelector(".active");
+    active.classList.remove("active");
+    active.innerHTML = "";
+    const five = document.querySelector(".five");
+    five.classList.add("active");
+    five.innerHTML = letter;
   }
 
   return (
@@ -179,7 +232,7 @@ export default function AppFunctional(props) {
         <div className="square two"></div>
         <div className="square three"></div>
         <div className="square four"></div>
-        <div className="square five active"></div>
+        <div className="square five active">{ letter }</div>
         <div className="square six"></div>
         <div className="square seven"></div>
         <div className="square eight"></div>
