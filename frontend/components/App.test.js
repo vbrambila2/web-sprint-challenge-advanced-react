bc673960af57b305a0dbf01ac4da64cb15377fb9
Victor Brambila <victorbrambila22@gmail.com>
2022-03-16 16:42:32 -0700

    all tests passing

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,12 +1,15 @@
 import AppFunctional from './AppFunctional';
 import React from 'react';
-import { render } from '@testing-library/react';
-// Write your tests here
+import { render, fireEvent, screen, queryByAttribute } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
-// Test that the visible texts in headings, buttons, links... render on the screen.
-//Test that typing on the input results in its value changing to the entered text.
+const emailInput = () => screen.getByPlaceholderText('type email');
+const getById = queryByAttribute.bind(null, 'id');
+const dom = render(<AppFunctional />);
+const submitBtn = getById(dom.container, 'submit');
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 beforeEach(() => {
@@ -14,5 +17,29 @@ beforeEach(() => {
 })
 
 describe('AppFunctional Component', () => {
-  test('Renders')
+  test('Renders left button', () => {
+    const leftBtn = screen.queryByText('LEFT');
+    expect(leftBtn).toBeVisible();
+    expect(leftBtn).toBeInTheDocument();
+  })
+  test('Renders right button', () => {
+    const rightBtn = screen.queryByText('RIGHT');
+    expect(rightBtn).toBeVisible();
+    expect(rightBtn).toBeInTheDocument();
+  })
+  test('Renders up button', () => {
+    const upBtn = screen.queryByText('UP');
+    expect(upBtn).toBeVisible();
+    expect(upBtn).toBeInTheDocument();
+  })
+  test('Renders down button', () => {
+    const downBtn = screen.queryByText('DOWN');
+    expect(downBtn).toBeVisible();
+    expect(downBtn).toBeInTheDocument();
+  })
+  test('We can change value of input', async () => {
+    fireEvent.change(emailInput(), { target: { value: 'lady@gaga.com' } });
+    fireEvent.click(submitBtn);
+    await screen.findByDisplayValue('lady@gaga.com');
+  })
 })
